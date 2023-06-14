8cbab35fe62aa8f3597fbd5462a24db9d35087ac
Shreyas <schicker@purdue.edu>
2023-06-13 22:58:05 -0500

    added identifiers

--- mysite/motion/models.py
+++ mysite/motion/models.py
@@ -11,10 +11,16 @@ class User(models.Model):
     height_in_inches = models.IntegerField(default=-1)
     weight_in_lbs = models.IntegerField(default=-1)
 
+    def __str__(self):
+        return self.id, self.first_name, self.last_name
+
 
 class Equipment(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.id, self.name
 
 class Exercise(models.Model):
     id = models.AutoField(primary_key=True)
@@ -22,10 +28,17 @@ class Exercise(models.Model):
     muscle = models.CharField(max_length=30)
     equipment = models.ManyToManyField(Equipment)
 
+    def __str__(self):
+        return self.id, self.name
+
 class WorkoutRoutine(models.Model):
     id = models.AutoField(primary_key=True)
+    routine_name = models.CharField(max_length=50)
     user_id = models.ManyToManyField(User)
     exercise_ids = models.ManyToManyField(Exercise)
+
+    def __str__(self):
+        return self.id, self.routine_name
     
 
 class Resource(models.Model):
@@ -33,3 +46,6 @@ class Resource(models.Model):
     name = models.CharField(max_length=30)
     link = models.CharField(max_length=10000)
     category = models.CharField(max_length=10000)
+
+    def __str__(self):
+        return self.id, self.name
--- mysite/nutri/models.py
+++ mysite/nutri/models.py
@@ -10,6 +10,9 @@ class User(models.Model):
     address = models.CharField(max_length=80)
     height_in_inches = models.IntegerField(default=-1)
     weight_in_lbs = models.IntegerField(default=-1)
+    
+    def __str__(self):
+        return self.id, self.first_name, self.last_name
 
 class Ingredient(models.Model):
     id = models.AutoField(primary_key=True)
@@ -19,11 +22,17 @@ class Ingredient(models.Model):
     macros_list = models.CharField(max_length=10000)
     recipe_count = models.IntegerField(default=-1)
 
+    def __str__(self):
+        return self.id, self.ingredient_name
+
 class Recipe(models.Model):
     id = models.AutoField(primary_key=True)
     recipe_name = models.CharField(max_length=30)
     source_link = models.CharField(max_length=10000)
     ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return self.id, self.recipe_name
 
 class Meal(models.Model):
     id = models.AutoField(primary_key=True)
@@ -32,8 +41,15 @@ class Meal(models.Model):
     recipes = models.ManyToManyField(Recipe)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.id, self.meal_name
+
 class Mealplan(models.Model):
     id = models.AutoField(primary_key=True)
+    plan_name = models.CharField(max_length=30)
     source_link = models.CharField(max_length=10000)
     meals = models.ManyToManyField(Meal)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id, self.plan_name
